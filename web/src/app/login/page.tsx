c405f32dbbd1c21e44c4c2f03737413920f6e747
tah5882 <me@tah5882.dev>
2026-01-31 16:43:09 +0900

    feat: ログインページを追加し、カスタムグローバルCSS定義を削除しました

---
+++ web/src/app/login/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { useState } from "react";
+import { authClient } from "@/lib/auth-client";
+
+export default function Login() {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	return (
+		<div>
+			<h1>Login</h1>
+			<p>Email</p>
+			<input
+				type="email"
+				value={email}
+				onChange={(e) => setEmail(e.target.value)}
+			/>
+			<p>Password</p>
+			<input
+				type="password"
+				value={password}
+				onChange={(e) => setPassword(e.target.value)}
+			/>
+			<button onClick={() => authClient.signIn.email({ email, password })}>
+				Login
+			</button>
+		</div>
+	);
+}
