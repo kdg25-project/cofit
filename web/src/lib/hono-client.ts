853651c13981ee7ccf8ae7ee045c3f02a724bec4
tah5882 <me@tah5882.dev>
2026-02-01 15:11:55 +0900

    feat: Honoクライアントが認証情報をリクエストに含めるよう設定を更新

--- web/src/lib/hono-client.ts
+++ web/src/lib/hono-client.ts
@@ -1,4 +1,8 @@
 import { hc } from "hono/client";
 import type { AppType } from "../../../api/src";
 
-export const client = hc<AppType>(process.env.NEXT_PUBLIC_API_URL!);
+export const client = hc<AppType>(process.env.NEXT_PUBLIC_API_URL!, {
+	init: {
+		credentials: "include",
+	},
+});
