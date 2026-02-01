dda03ac6716165b67acac8a46db5073e8b2a2593
tah5882 <me@tah5882.dev>
2026-02-01 11:31:48 +0900

    chore: APIクライアントのURLを環境変数から取得するように変更

--- web/src/lib/hono-client.ts
+++ web/src/lib/hono-client.ts
@@ -1,4 +1,4 @@
 import { hc } from "hono/client";
 import type { AppType } from "../../../api/src";
 
-export const client = hc<AppType>("http://localhost:8787");
+export const client = hc<AppType>(process.env.NEXT_PUBLIC_API_URL!);
