68f8cf9d9908efe8319152201586eda7adfdb972
tahoito <taho.ito0304@gmail.com>
2026-02-02 18:57:52 +0900

    計測画面

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -29,8 +29,6 @@ export default function RootLayout({
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
 				{children}
-
-				<BottomNav />
 			</body>
 		</html>
 	);
